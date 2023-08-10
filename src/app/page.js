91fdbfaa017aec8d8d4629f9934806cf521d5384
thanaschai <thanaschai@gmail.com>
2023-08-10 15:26:45 +0700

    done

--- src/app/page.js
+++ src/app/page.js
@@ -9,77 +9,38 @@ export default function HomePage() {
       {/* Entire App Container */}
       <div
         style={{ maxWidth: "700px" }}
-        className="mx-auto p-3 rounded rounded-3 shadow-sm bg-white"
+        className="mx-auto p-3 rounded rounded-3
+         shadow-sm bg-white"
       >
         {/* Post Owner Example*/}
-        <div className="vstack gap-3">
-          <div className="d-flex align-items-center gap-3">
-            <img
-              src="/profileImages/handsome.jpg"
-              width="48"
-              height="48"
-              className="rounded-circle"
-              style={{ objectFit: "cover" }}
-            />
-            <span className="fw-semibold fs-5">Chayanin Suatap 650610560</span>
-          </div>
 
-          <span>Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม #261207</span>
-
-          <div className="d-flex align-items-center gap-1">
-            <img src="/like.svg" width={20}></img>
-            <span className="text-muted">100 คน</span>
-          </div>
-          <hr className="m-0 border" />
-        </div>
+        <PostOwner
+          imgSrc="/profileImages/001.jpg"
+          username="thanaschai Chanabua 650610769"
+          postText="Quiz ง่ายจังเลยครับ ขอยาก ๆ กว่านี้ได้ไหม 
+        #261207"
+          likeNum={100}
+        />
 
         {/* Comment Example */}
-        <div className="d-flex gap-2 my-2">
-          <img
-            src="/profileImages/lisa.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">Lisa</span>
-            <br />
-            <span>จริงค่า</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">999 คน</span>
-            </div>
-          </div>
-        </div>
+
+        {/* <Comment userImagePath="/profileImages/lisa.jpg" username="Lisa" commentText="จริงค่า" likeNum={999} /> */}
 
         {/* Reply Example */}
-        <div className="d-flex gap-2 my-2 ps-5">
-          <img
-            src="/profileImages/puppy.jpg"
-            width="48"
-            height="48"
-            className="rounded-circle"
-            style={{ objectFit: "cover" }}
-          />
-          <div
-            className="rounded rounded-3 p-2"
-            style={{ backgroundColor: "#E5E7EB" }}
-          >
-            <span className="fw-semibold">หมาน้อย</span>
-            <br />
-            <span>จริงค้าบบบบบบบบ</span>
-            <div className="d-flex align-items-center gap-1">
-              <img src="/like.svg" width={20}></img>
-              <span className="text-muted">2 คน</span>
-            </div>
-          </div>
-        </div>
+
+        {/* <Reply userImagePath="/profileImages/puppy.jpg" username="หมาน้อย" replyText="จริงค้าบบบบบบบบ" likeNum={2} /> */}
 
         {/* map-loop render Comment component here */}
+        {comments.map((comment) => (
+          <Comment
+            key={comment.commentText}
+            userImagePath={comment.userImagePath}
+            username={comment.username}
+            commentText={comment.commentText}
+            likeNum={comment.likeNum}
+            replies={comment.replies}
+          />
+        ))}
       </div>
     </div>
   );
--- src/components/PostOwner.jsx
+++ src/components/PostOwner.jsx
@@ -1,3 +1,27 @@
-export const PostOwner = ({}) => {
-  return <div>Your code here...</div>;
+import Image from "next/image";
+
+export const PostOwner = ({ imgSrc, username, postText, likeNum }) => {
+  return (
+    <div className="vstack gap-3">
+      <div className="d-flex align-items-center gap-3">
+        <Image
+          src={imgSrc}
+          width={48}
+          height={48}
+          className="rounded-circle"
+          style={{ objectFit: "cover" }}
+          alt="Post Owner Profile Picture"
+        />
+        <span className="fw-semibold fs-5">{username}</span>
+      </div>
+
+      <span>{postText}</span>
+
+      <div className="d-flex align-items-center gap-1">
+        <Image src="/like.svg" width={20} height={20} alt="Like" />
+        <span className="text-muted">{likeNum} คน</span>
+      </div>
+      <hr className="m-0 border" />
+    </div>
+  );
 };
